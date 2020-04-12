4eed1fecc4614cf02542352b12d21862e0ecbef1
toplenboren <toplenboren@yandex.ru>
2020-04-12 19:05:28 +0500

    Refactor Teams controller, add migrations

---
+++ Quest.DAL/Migrations/20200411234324_updateTeamsAndQuests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quest.DAL.Migrations
+{
+    public partial class updateTeamsAndQuests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InviteTokenSecret",
+                table: "Teams",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxTeamSize",
+                table: "Quests",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InviteTokenSecret",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "MaxTeamSize",
+                table: "Quests");
+        }
+    }
+}
