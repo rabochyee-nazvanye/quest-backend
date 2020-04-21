82f6139d4eec58ebc290a7cbef15164af2314b3c
usernamedt <usernamedt@protonmail.ch>
2020-04-21 19:34:16 +0500

    Added query for quest tasks Refactored query for get team request

--- Quest.Application/Tasks/Queries/GetQuestTasksQuery.cs
+++ Quest.Application/Tasks/Queries/GetQuestTasksQuery.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MediatR;
+using Quest.Domain.Models;
 
 namespace Quest.Application.Tasks.Queries
 {
-    class GetQuestTasksQuery
+    public class GetQuestTasksQuery : IRequest<BaseResponse<List<TaskEntity>>>
     {
+        public GetQuestTasksQuery(string userId,int questId)
+        {
+            QuestId = questId;
+            UserId = userId;
+        }
+        
+        public string UserId { get; set; }
+        public int QuestId { get; set; }
     }
 }
