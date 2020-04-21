689959dfd1cd02947332cb5ea9f6471e41119975
usernamedt <usernamedt@protonmail.ch>
2020-04-21 19:32:48 +0500

    Fixed relation between Tasks and Teams

---
+++ Quest.DAL/Migrations/20200421143002_QuestTasksFix.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quest.DAL.Migrations
+{
+    public partial class QuestTasksFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskAttemptTeams");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskAttemptTeams",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(type: "integer", nullable: false),
+                    TaskId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskAttemptTeams", x => new { x.TeamId, x.TaskId });
+                    table.ForeignKey(
+                        name: "FK_TaskAttemptTeams_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskAttemptTeams_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskAttemptTeams_TaskId",
+                table: "TaskAttemptTeams",
+                column: "TaskId");
+        }
+    }
+}
